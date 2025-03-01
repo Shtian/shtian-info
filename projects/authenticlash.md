ad09e7e2fbb6c0433a8a747462937ec16537b259
Stian Haga <hagastian@gmail.com>
2025-03-01 22:09:47 +0100

    feat: add project content collection and layout

---
+++ projects/authenticlash.md
@@ -0,0 +1,15 @@
+---
+name: Authenticlash
+description: "This is some describing text"
+date: 2024-01-01
+slug: authenticlash
+link: https://www.github.com/shtian/authenticlash
+tags:
+- SvelteKit
+- Supabase
+- Game
+---
+
+# Authenticlash
+
+This is one project
---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { file, glob } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const projects = defineCollection({ loader: glob({pattern: "projects/*.md"}), schema: z.object({
+    name: z.string(),
+    description: z.string(),
+    tags: z.array(z.string()),
+    date: z.date(),
+    link: z.string().url()
+})});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { projects };
